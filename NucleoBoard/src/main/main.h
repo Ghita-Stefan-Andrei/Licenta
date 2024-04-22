51bfaefd064afb0ce73d81fb75cad5e951fa2f63
Ghita Stefan-Andrei <stefststef72@gmail.com>
2024-04-22 21:21:47 +0300

    implemented NTP client

---
+++ NucleoBoard/src/main/main.h
@@ -0,0 +1,40 @@
+#pragma once
+#define TIME_ZONE_OFFSET_HRS (3)
+
+void initEthernet()
+{
+#if !(USE_BUILTIN_ETHERNET || USE_UIP_ETHERNET)
+
+  ET_LOGWARN3(F("Board :"), BOARD_NAME, F(", setCsPin:"), USE_THIS_SS_PIN);
+
+  ET_LOGWARN(F("Default SPI pinout:"));
+  ET_LOGWARN1(F("MOSI:"), MOSI);
+  ET_LOGWARN1(F("MISO:"), MISO);
+  ET_LOGWARN1(F("SCK:"),  SCK);
+  ET_LOGWARN1(F("SS:"),   SS);
+  ET_LOGWARN(F("========================="));
+
+  // For other boards, to change if necessary
+  #if ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+    // Must use library patch for Ethernet, Ethernet2, EthernetLarge libraries
+    Ethernet.init (USE_THIS_SS_PIN);
+   
+  #elif USE_CUSTOM_ETHERNET
+    // You have to add initialization for your Custom Ethernet here
+    // This is just an example to setCSPin to USE_THIS_SS_PIN, and can be not correct and enough
+    //Ethernet.init(USE_THIS_SS_PIN);
+  
+  #endif  //( ( USE_ETHERNET_GENERIC || USE_ETHERNET_ENC )
+#endif
+
+  // start the ethernet connection and the server:
+  // Use DHCP dynamic IP and random mac
+  uint16_t index = millis() % NUMBER_OF_MAC;
+  // Use Static IP
+  //Ethernet.begin(mac[index], ip);
+  Ethernet.begin(mac[index]);
+
+  // you're connected now, so print out the data
+  Serial.print(F("You're connected to the network, IP = "));
+  Serial.println(Ethernet.localIP());  
+}
---
+++ NucleoBoard/src/main/main.ino
@@ -0,0 +1,44 @@
+#include "defines.h"
+#include "main.h"
+#include <NTPClient_Generic.h>
+
+EthernetUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial && millis() < 5000);
+
+  Serial.print(F("\nStart Ethernet_NTPClient_Basic_STM32 on ")); Serial.print(BOARD_NAME);
+  Serial.print(F(" with ")); Serial.println(SHIELD_TYPE);
+  Serial.println(ETHERNET_WEBSERVER_STM32_VERSION);
+  Serial.println(NTPCLIENT_GENERIC_VERSION);
+
+  initEthernet();
+
+  timeClient.begin();
+  timeClient.setTimeOffset(3600 * TIME_ZONE_OFFSET_HRS);
+  // default 60000 => 60s. Set to once per hour
+  timeClient.setUpdateInterval(SECS_IN_HR);
+  
+  Serial.println("Using NTP Server " + timeClient.getPoolServerName());
+
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  timeClient.update();
+
+
+  if (timeClient.updated())
+    Serial.println("********UPDATED********");
+  else
+    Serial.println("******NOT UPDATED******");
+
+    
+  // Without leading 0
+  Serial.println(String("LOC : ") + timeClient.getHours() + ":" + timeClient.getMinutes() + ":" + timeClient.getSeconds() + " " +
+        timeClient.getDoW() + " " + timeClient.getDay() + "/" + timeClient.getMonth() + "/" + timeClient.getYear() + " or " +
+        timeClient.getDay() + " " + timeClient.getMonthStr() + " " + timeClient.getYear());  
+  delay(10000);
+}
