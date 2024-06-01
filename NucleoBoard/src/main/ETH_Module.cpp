b25f1acda11773f01232e467a133d9f44cdb5572
Ghita Stefan-Andrei <stefststef72@gmail.com>
2024-06-02 01:10:10 +0300

    updated ETH_Module to support the led driver

--- NucleoBoard/src/main/ETH_Module.cpp
+++ NucleoBoard/src/main/ETH_Module.cpp
@@ -20,12 +20,40 @@ EthernetLinkStatus EthernetModule::getStatus()
     return this->status;
 }
 
+EthernetLinkStatus EthernetModule::getLastStatus()
+{
+    return this->lastStatus;
+}
+
 void EthernetModule::handleEthernetConnectionChanges()
 {
     switch(status)
     {
-        case LinkOFF: digitalWrite(LED_RED, HIGH);  break;  // Light up the red LED when the connection is broken
+        case LinkOFF: LedDriver::openEtcConLed();  break;  // Light up the red LED when the connection is broken
         case LinkON : NVIC_SystemReset(); break;  // Reset the board to try to reconnect to the internet after an Ethernet connection is reestablished
         default:break;
     }
+}
+
+EthernetModule::EthernetModule()
+{
+    this->ip = new BYTE[IP_BYTE_LENGTH];
+}
+
+EthernetModule::~EthernetModule()
+{
+    delete[] this->ip;
+}
+
+void EthernetModule::decodeIP(uint32_t rawIP)
+{
+  this->ip[0] = (rawIP >>  0) & ISOLATE_LAST_BYTE;
+  this->ip[1] = (rawIP >>  8) & ISOLATE_LAST_BYTE;
+  this->ip[2] = (rawIP >> 16) & ISOLATE_LAST_BYTE;
+  this->ip[3] = (rawIP >> 24) & ISOLATE_LAST_BYTE;
+}
+
+BYTE* EthernetModule::getIPAddress()
+{
+    return this->ip;
 }
--- NucleoBoard/src/main/ETH_Module.h
+++ NucleoBoard/src/main/ETH_Module.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <STM32Ethernet.h>
+#include "LED_Driver.h"
+#include "COM_Protocol_Defines.h"
+
+#define ISOLATE_LAST_BYTE 0x000000FF
 
 typedef struct
 {
@@ -12,7 +16,26 @@ class EthernetModule
     public:
         EthernetLinkStatus status;
         EthernetLinkStatus lastStatus;
+        BYTE* ip;
     public:
+        /**
+          * @brief Constructor for EthernetModule class.
+          *
+          * Initializes the IP address array with a new dynamic memory allocation.
+          * The size of the array is defined by the constant IP_BYTE_LENGTH.
+          *
+          * @return None
+          */
+        EthernetModule();
+
+        /**
+          * @brief Destructor for EthernetModule class.
+          *
+          * This destructor is responsible for freeing the dynamically allocated memory for the IP address.
+          * It is called when an object of EthernetModule class goes out of scope or is explicitly deleted.
+          */
+        ~EthernetModule();
+
         /**
           * @brief Updates the current Ethernet link status.
           *
@@ -23,6 +46,7 @@ class EthernetModule
           * @return void
           */
         void updateLinkStatus(const EthernetLinkStatus& ethStatus);
+
         /**
           * @brief Sets the last recorded Ethernet link status.
           *
@@ -33,6 +57,7 @@ class EthernetModule
           * @return void
           */
         void setLastStatus(const EthernetLinkStatus& ethStatus);
+
         /**
           * @brief Handles the actions to be taken when the Ethernet connection status changes.
           *
@@ -43,6 +68,7 @@ class EthernetModule
           * @return void
           */
         void handleEthernetConnectionChanges();
+
         /**
           * @brief Checks if the Ethernet status has changed since the last update.
           *
@@ -52,6 +78,7 @@ class EthernetModule
           * It is used to determine if any changes have occurred in the Ethernet connection.
           */
         bool ethStatusChanged();
+
         /**
           * @brief Get the current Ethernet link status.
           *
@@ -60,4 +87,51 @@ class EthernetModule
           * @return EthernetLinkStatus The current Ethernet link status.
           */
         EthernetLinkStatus getStatus();
+
+        /**
+        * @brief Get the last recorded Ethernet link status.
+        *
+        * This function returns the last recorded Ethernet link status.
+        *
+        * @return EthernetLinkStatus The last recorded Ethernet link status.
+        */
+        EthernetLinkStatus getLastStatus();
+
+        /**
+          * @brief Decodes a 32-bit raw IP address into an array of 4 bytes.
+          *
+          * This function takes a 32-bit raw IP address and decodes it into an array of 4 bytes.
+          * The raw IP address is assumed to be in network byte order (big-endian).
+          *
+          * @param rawIP The 32-bit raw IP address to decode.
+          *
+          * @return void. The decoded IP address is stored in the 'ip' member variable of the EthernetModule object.
+          *
+          * @note The 'ip' member variable is assumed to be a dynamically allocated array of 4 bytes.
+          *
+          * @warning This function does not perform any error checking or validation on the input 'rawIP'.
+          *          It assumes that the input is a valid 32-bit raw IP address.
+          */
+        void decodeIP(uint32_t rawIP);
+
+        /**
+          * @brief Get the IP address of the Ethernet module.
+          *
+          * This function returns a pointer to the array that holds the IP address.
+          * The IP address is stored in network byte order (big endian).
+          *
+          * @return BYTE* Pointer to the array that holds the IP address.
+          *
+          * @note The returned pointer points to the internal data of the EthernetModule object.
+          *       Do not modify or delete the memory pointed by the returned pointer.
+          *
+          * @note The IP address is updated by the decodeIP function.
+          *
+          * @note The IP address is stored in the following format:
+          *       ip[0] = first byte of the IP address
+          *       ip[1] = second byte of the IP address
+          *       ip[2] = third byte of the IP address
+          *       ip[3] = fourth byte of the IP address
+          */
+        BYTE* getIPAddress();
 };
