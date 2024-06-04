3c07aa68c606c7a249e7a5770e0a37c62ca90aac
Ghita Stefan-Andrei <stefststef72@gmail.com>
2024-06-04 23:42:02 +0300

    added documantation

--- NucleoBoard/src/main/LED_Driver.cpp
+++ NucleoBoard/src/main/LED_Driver.cpp
@@ -1,31 +1,38 @@
 #include "LED_Driver.h"
+
+LedDriver::LedDriver()
+{
+    this->initialized = false;
+}
 void LedDriver::init()
 {
     pinMode(ETH_CONNECTION_LED,  OUTPUT);    //the RED led marks the connection to ethernet. If it is on, the board is not connected to ethernet.
     pinMode(LOOP_FUNCTION_ACTIVE_LED, OUTPUT);    //the BLUE led marks when the board enters the loop function
+    this->initialized = true;
 }
 
 void LedDriver::displayEthStatus(const EthernetLinkStatus& ethStatus)
 {
-    switch(ethStatus)
-    {
-        case LinkON: closeEthConLed(); break;
-        case LinkOFF: openEthConLed(); break;
-        default: break;
-    }
+    if (this->initialized)
+        switch(ethStatus)
+        {
+            case LinkON: closeEthConLed(); break;
+            case LinkOFF: openEthConLed(); break;
+            default: break;
+        }
 }
 
 void LedDriver::closeEthConLed()
 {
-    digitalWrite(ETH_CONNECTION_LED, LOW);
+    if (this->initialized) digitalWrite(ETH_CONNECTION_LED, LOW);
 }
 
 void LedDriver::openEthConLed()
 {
-    digitalWrite(ETH_CONNECTION_LED, HIGH);
+    if (this->initialized) digitalWrite(ETH_CONNECTION_LED, HIGH);
 }
 
 void LedDriver::enteredLoopLed()
 {
-    digitalWrite(LOOP_FUNCTION_ACTIVE_LED, HIGH);
+    if (this->initialized) digitalWrite(LOOP_FUNCTION_ACTIVE_LED, HIGH);
 }
--- NucleoBoard/src/main/LED_Driver.h
+++ NucleoBoard/src/main/LED_Driver.h
@@ -8,11 +8,81 @@
 class LedDriver
 {
     private:
-
+        bool initialized;
     public:
+        /**
+          * @brief Constructor for LedDriver class.
+          *
+          * Initializes the LedDriver object and sets the initialized flag to false.
+          *
+          * @return None
+          */
+        LedDriver();
+
+       /**
+         * @brief Initializes the LED pins for the Ethernet and Loop Function status.
+         *
+         * This function sets the pin modes for the LED pins to OUTPUT. The ETH_CONNECTION_LED
+         * is used to indicate the connection status to the Ethernet. If the LED is on, the board
+         * is not connected to Ethernet. The LOOP_FUNCTION_ACTIVE_LED is used to indicate when
+         * the board enters the loop function.
+         *
+         * @return void
+         */
         void init();
+        /**
+          * @brief This function is used to display the Ethernet link status on the LED.
+          *
+          * @param ethStatus The current Ethernet link status.
+          *
+          * @return void
+          *
+          * @details This function takes the Ethernet link status as input and
+          *          controls the LED accordingly. If the link status is LinkON,
+          *          the Ethernet connection LED is turned off. If the link status
+          *          is LinkOFF, the Ethernet connection LED is turned on.
+          *          If the link status is neither LinkON nor LinkOFF, no action is taken.
+          */
         static void displayEthStatus(const EthernetLinkStatus& ethStatus);
+
+        /**
+          * @brief Closes the Ethernet connection LED.
+          *
+          * This function sets the pin connected to the Ethernet connection LED to low,
+          * effectively turning it off. This indicates that the board is connected to the
+          * Ethernet.
+          *
+          * @return void
+          *
+          * @note This function should only be called after the LedDriver has been
+          * initialized using the init() method.
+          *
+          * @see init()
+          * @see openEthConLed()
+          */
         static void closeEthConLed();
+
+        /**
+          * @brief Function to turn on the Ethernet connection LED.
+          *
+          * This function sets the pin connected to the Ethernet connection LED to HIGH,
+          * indicating that the board is not connected to the Ethernet.
+          *
+          * @return void
+          *
+          * @note This function should be called only after the LedDriver object has been initialized.
+          *
+          * @see init()
+          * @see closeEthConLed()
+          */
         static void openEthConLed();
+
+        /**
+          * @brief Function to indicate that the board has entered the loop function.
+          *
+          * This function sets the LOOP_FUNCTION_ACTIVE_LED pin to HIGH, indicating that the board is in the loop function.
+          *
+          * @return void
+          */
         static void enteredLoopLed();
 };
