8cad6b93331ac6063f6bf9bde3b319b3b17b91c1
Ghita Stefan-Andrei <stefststef72@gmail.com>
2024-06-15 02:25:43 +0300

    added init check

--- NucleoBoard/src/main/LED_Driver.cpp
+++ NucleoBoard/src/main/LED_Driver.cpp
@@ -1,18 +1,19 @@
 #include "LED_Driver.h"
 
+bool LedDriver::initialized = false;
 LedDriver::LedDriver()
 {
-    this->initialized = false;
+    initialized = false;
 }
 void LedDriver::init()
 {
-    pinMode(ETH_CONNECTION_LED,  OUTPUT);    //the RED led marks the connection to ethernet. If it is on, the board is not connected to ethernet.
-    pinMode(LOOP_FUNCTION_ACTIVE_LED, OUTPUT);    //the BLUE led marks when the board enters the loop function
-    this->initialized = true;
+    pinMode(ETH_CONNECTION_LED,  OUTPUT);    
+    pinMode(LOOP_FUNCTION_ACTIVE_LED, OUTPUT);    
+    initialized = true;
 }
 
 void LedDriver::displayEthStatus(const EthernetLinkStatus& ethStatus)
-{
+{   if(initialized)
     switch(ethStatus)
     {
         case LinkON: closeEthConLed(); break;
@@ -23,15 +24,18 @@ void LedDriver::displayEthStatus(const EthernetLinkStatus& ethStatus)
 
 void LedDriver::closeEthConLed()
 {
+    if(initialized)
     digitalWrite(ETH_CONNECTION_LED, LOW);
 }
 
 void LedDriver::openEthConLed()
 {
+    if(initialized)
     digitalWrite(ETH_CONNECTION_LED, HIGH);
 }
 
 void LedDriver::enteredLoopLed()
 {
+    if(initialized)
     digitalWrite(LOOP_FUNCTION_ACTIVE_LED, HIGH);
 }
